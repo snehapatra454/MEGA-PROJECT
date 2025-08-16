1d37559b4cc3f3cbb2c78ed615ef9850e8ce4376
Sneha Patra <snehapatra358@gmail.com>
2025-08-16 13:18:18 +0530

    utils code updated

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// const asyncHandler = () => {}
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+export {asyncHandler}
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+
+//     } catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
